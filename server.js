c29840e46464425a8c1fb55a4ada89f0f818782d
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-01 10:16:00 -0500

    Updated to use express.Router for better structure.

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+const router = express.Router();
+
+router.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
+
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
