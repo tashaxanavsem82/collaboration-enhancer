42a89a23b9dca7364453db3630779c0de06811cd
tashaxanavsem82 <swgwull@hotmail.com>
2025-10-01 10:13:30 -0500

    Fix: Update express.static to support path and options.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static('public', { index: false }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
